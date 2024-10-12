5d16ec7690e70a1ffb6a93939e351f9e4125d37b
Pracheta-Bagde <prachetabagde2003@gmail.com>
2024-10-12 16:38:52 +0530

    Initial commit

---
+++ src/main/java/com/example/securingAWebApplication/MvcConfigure.java
@@ -0,0 +1,16 @@
+package com.example.securingAWebApplication;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+//MVC - Model,View , Controller
+@Configuration
+public class MvcConfigure implements WebMvcConfigurer {
+    public void addViewControllers(ViewControllerRegistry registry){
+        registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/").setViewName("home");
+        registry.addViewController("/hello").setViewName("hello");
+        registry.addViewController("/login").setViewName("login");
+    }
+}
